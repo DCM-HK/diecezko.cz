d7486f7310bdfae9b14a2b0e20935e155b58c2dc
vlachvojta <vojtik.vlach@seznam.cz>
2026-03-28 14:05:19 +0100

    fix feedback link

--- components/switcher/diecezko-info.js
+++ components/switcher/diecezko-info.js
@@ -68,7 +68,7 @@ export default function DiecezkoInfo({ id, className }) {
             </button>
           </Link> */}
           <Link
-            href={`https://forms.office.com/Pages/ResponsePage.aspx?id=0yfGPzCmsUOBrkMeHpEoRlGhSFMKQDJHlR6vccQ-P5tURFQ5MUs4VUgxUzVHSDZWSzhURDE2VUlUWC4u&embed=true`}
+            href={`https://forms.office.com/Pages/ResponsePage.aspx?id=0yfGPzCmsUOBrkMeHpEoRlGhSFMKQDJHlR6vccQ-P5tUNTBQVEVBMVEzVTBSRVdTUlJDNTBHMUVVWi4u`}
             target="_blank"
             className="border col-span-2 border-[#3b3b3b] hover:bg-[#3b3b3b] rounded-full p-2 px-5 text-base flex flex-row items-center justify-center"
           >
