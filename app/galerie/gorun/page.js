68cb1c3e783f1bdf5a492a4af52db8a4c9dd51a2
vlachvojta <vojtik.vlach@seznam.cz>
2026-04-06 09:44:59 +0200

    update gallery with more links to Matouš socials

--- app/galerie/gorun/page.js
+++ app/galerie/gorun/page.js
@@ -16,6 +16,15 @@ import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import Lightbox from "yet-another-react-lightbox";
 import PhotoAlbum from "react-photo-album";
+import ContainerUl from "../../../components/layouts/container-ul";
+import ContainerLi from "../../../components/layouts/container-li";
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandYoutube,
+  IconPhoto,
+  IconCamera,
+} from "@tabler/icons";
 
 export default function Galerie() {
   const [index, setIndex] = useState(-1);
@@ -34,21 +43,14 @@ export default function Galerie() {
               className="underline"
             >
               Matouš �orun
-            </Link>. Jeho další tvorba je dostupná třeba{" "}
-            <Link
-              href={`https://eu.zonerama.com/matousgorun/1348342`}
-              target="_blank"
-              rel="external noopener nofollow"
-              className="underline"
-            >
-              tady
             </Link>.
+            <MatousLinks />
             <p className="my-2 w-full"></p>
             <PhotoAlbum
               layout="masonry"
               photos={photos_gorun}
               onClick={({ index: current }) => setIndex(current)}
-            />
+              />
             <Lightbox
               open={index >= 0}
               index={index}
@@ -58,10 +60,53 @@ export default function Galerie() {
               counter={{ container: { style: { top: "unset", bottom: 0 } } }}
               thumbnails={{ border: 0, gap: 4 }}
             />
+            <MatousLinks />
           </ContainerDescription>
         </Container>
       </PageMain>
       <Footer />
     </main>
+  );
+}
+
+
+export function MatousLinks() {
+  return (
+    <ContainerUl className={`!list-none !pl-0 mb-6`}>
+      <ContainerLi className="mt-2">
+        <Link
+          href={`https://facebook.com/vesmirnakapela`}
+          target="_blank"
+          rel="external noopener nofollow"
+          title="Facebook"
+        >
+          <IconBrandFacebook className="inline mr-2" size={30} />
+        </Link>
+        <Link
+          href={`https://www.instagram.com/matous_gorun/`}
+          target="_blank"
+          rel="external noopener nofollow"
+          title="Instagram"
+        >
+          <IconBrandInstagram className="inline mr-2" size={30} />
+        </Link>
+        <Link
+          href={`https://eu.zonerama.com/matousgorun/1348342`}
+          target="_blank"
+          rel="external noopener nofollow"
+          title="Webové stránky"
+        >
+          <IconCamera className="inline mr-2" size={30} />
+        </Link>
+        <Link
+          href={`https://www.youtube.com/@matousgorun9082`}
+          target="_blank"
+          rel="external noopener nofollow"
+          title="Youtube kanál"
+        >
+          <IconBrandYoutube className="inline mr-2" size={30} />
+        </Link>
+      </ContainerLi>
+    </ContainerUl>
   );
 }
